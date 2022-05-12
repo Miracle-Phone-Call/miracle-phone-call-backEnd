d2b039faf1d53cc02946c8fb4fc4180538ebceef
Gurupriyan <gurupriyan98@gmail.com>
2022-05-11 22:44:38 -0400

    Created basic server that is connected to pg db and set up with migration files

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require('express')
+
+
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json("Hello")
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Server running on ", process.env.PORT)
+})
---
+++ migrations/20220512022836_create_chat_room_table.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.up = function(knex) {
+    return knex.schema.createTable('chat_room', function (table) {
+        table.increments("id").primary();
+        table.integer('relationship_id').references('id').inTable("relationships");
+      })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable("chat_room")
+};

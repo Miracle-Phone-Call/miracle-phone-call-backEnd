39ad524a02ff68ced994a031714dd031336a869c
Pedro Delgado <pdelgado1374@gmail.com>
2022-05-17 12:05:42 -0400

    CRUD operations for user profile

--- delete-request.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require("pg")
-
-//DELETE THE USER ACCOUNT
-app.delete("/users/username", async(req, res) => {
-  const userName = req.params.username
-  const delteUsername = await Pool.query('DELETE FROM users WHERE username = $1', [userName])
-  res.status(200).json(delteUsername)
-})
--- request.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require("pg")
-
-// Get first name
-app.patch("/users/username",async(req,res) => {
-  const firstName = req.body
-  const userName = req.params.username
-  const updateFirstName = await Pool.query('UPDATE users SET first_name = $1 WHERE username = $2 RETURNING *', [firstName,userName]).then(results => results.rows[0])
-  res.status(200).json(updateFirstName)
-})

4c20a86739af57514e049267e95ef7eb78cb3239
Gurupriyan <gurupriyan98@gmail.com>
2022-05-26 12:51:19 -0400

    added chat feature and update bio feature

--- migrations/20220512022726_create_user_table.js
+++ migrations/20220512022726_create_user_table.js
@@ -8,6 +8,7 @@
         table.string('first_name').notNullable();
         table.string('last_name').notNullable();
         table.string('username').unique().notNullable();
+        table.string('bio').nullable();
         table.string('password').notNullable();
       })
 };
